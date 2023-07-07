8719cece5b01f26ff1be8449dcb566b402cb77e6
rabbi50 <jasrabbi50@gmail.com>
2023-07-08 01:57:16 +0600

    update gitignore file

--- main.py
+++ main.py
@@ -22,10 +22,8 @@ def post_data():
             iso_time = convert_timestamp_to_iso(int(time))
             new_reading = Reading(iso_time, name, float(value))
             key = f"{new_reading.time}_{new_reading.value}"
-            print("new reading timestamp:", new_reading.time)
             add_reading(key, new_reading)
         except (ValueError, TypeError) as e:
-            print("except error:", e)
             response = {'success': False, 'message': 'Malformed data'}
             return jsonify(response), 400
 
@@ -34,66 +32,19 @@ def post_data():
 
 
 
-def avg_power_reading(filtered_data):
-    result = []
-    current_sum = 0
-    power_sum = 0
-    count = 0
-    prev_date = None
 
-    for reading in filtered_data:
-        date = reading['time']
-        if prev_date is None:
-            prev_date = date
-
-        if date != prev_date:
-            if count > 0:
-                avg_current = current_sum / count
-                avg_power = power_sum / count
-                avg_power_reading = avg_current * avg_power
-
-                result.append({
-                    'date': prev_date,
-                    'average_power_reading': avg_power_reading
-                })
-
-            current_sum = 0
-            power_sum = 0
-            count = 0
-            prev_date = date
-
-        current_sum += reading['value'] if reading['name'].lower() == 'current' else 0
-        power_sum += reading['value'] if reading['name'].lower() == 'power' else 0
-        count += 1
-
-    if count > 0:
-        avg_current = current_sum / count
-        avg_power = power_sum / count
-        avg_power_reading = avg_current * avg_power
-
-        result.append({
-            'date': prev_date,
-            'average_power_reading': avg_power_reading
-        })
-    return result
 
 @app.route('/data', methods=['GET'])
 def get_data():
     from_date = date.fromisoformat(request.args.get('from'))
     to_date = date.fromisoformat(request.args.get('to'))
+
     if not from_date or not to_date:
         return 'Please provide both "from" and "to" parameters.', 400
     filtered_data = [
-        get_reading(key).__dict__ for key, reading in database.items()
+        reading.__dict__ for reading in database.values()
         if from_date <= datetime.fromisoformat(reading.time).date() <= to_date
     ]
-
-    print('filtered data:', filtered_data)
-    
-    avg_power_reading_list = avg_power_reading(filtered_data)
-
-    
-    print('avg power reading list:', avg_power_reading_list)
 
 
     return jsonify(filtered_data + avg_power_reading_list)
